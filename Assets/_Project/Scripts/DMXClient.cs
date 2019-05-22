7ec3618b3e1714f4a71492393dfc7500b0f837fb
Surya Buchwald <surya@heliosinteractive.com>
2019-05-22 14:09:34 -0700

    update DMXClient to send HTTP GET requests instead of WebSockets

--- Assets/_Project/Scripts/DMXClient.cs
+++ Assets/_Project/Scripts/DMXClient.cs
@@ -1,71 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using WebSocketSharp;
+﻿using UnityEngine;
+using UnityEngine.Networking;
 
 namespace MMM.DMX
 {
     public class DMXClient : MonoBehaviour
     {
 
-        private WebSocket client;
         [SerializeField] private string address = "localhost";
         [SerializeField] private int port = 4649;
 
-        private void OnEnable()
-        {
-            string url = $"ws://{address}:{port}/DMX";
-            client = new WebSocket(url);
-
-            client.OnOpen += (sender, e) =>
-            {
-                Debug.Log("Client Opened Connection");
-            };
-
-            client.OnMessage += OnMessage;
-
-            client.OnError += (sender, e) =>
-            {
-                Debug.LogError($"Client got error {e.Message}");
-            };
-
-            client.OnClose += (sender, e) =>
-            {
-                Debug.Log("Client Closed Connection");
-            };
-
-            Invoke("Connect", 1);
-        }
-
-        private void Connect()
-        {
-            client.Connect();
-        }
-
-        private void OnMessage(object sender, MessageEventArgs e)
-        {
-            Debug.Log($"Client got message {e.Data}");
-        }
-
-        private void OnDisable()
-        {
-            if (client.IsConnected) client.Close();
-        }
-
-        [SerializeField] private Color onColor;
-
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.UpArrow))
+            if (Input.GetMouseButtonDown(0))
             {
-                client.Send(JsonUtility.ToJson(onColor));
+                var color = Random.ColorHSV(0, 1, 1, 1, 1, 1);
+                SendColor(color);
+                Camera.main.backgroundColor = color;
             }
 
-            if (Input.GetKeyDown(KeyCode.DownArrow))
+            if (Input.GetMouseButtonUp(0))
             {
-                client.Send(JsonUtility.ToJson(Color.clear));
+                var color = Color.clear;
+                SendColor(color);
+                Camera.main.backgroundColor = color;
             }
         }
+
+        public void SendColor(Color32 color)
+        {
+            UnityWebRequest webRequest = UnityWebRequest.Get(GetQueryForColor(color));
+            webRequest.SendWebRequest();
+        }
+
+        public string GetQueryForColor(Color32 color)
+        {
+            return $"http://{address}:{port}/dmx?r={color.r}&g={color.g}&b={color.b}&w={color.a}";
+        }
+
     }
 
 }
